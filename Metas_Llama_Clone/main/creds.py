33b8a029c6bd9102ccfdeedddf4a3b0cc57af94e
Addisu Mengistu <146175464+Crackingastro@users.noreply.github.com>
2024-01-16 22:36:18 +0300

    removed creds.py and gitignore

--- Metas_Llama_Clone/main/creds.py
+++
@@ -1,9 +0,0 @@
-# Creds
-
-url = "https://meta-llama-fast-api.p.rapidapi.com/mistralchat"
-headers = {
-            "content-type": "application/json",
-            "Content-Type": "application/json",
-            "X-RapidAPI-Key": "ed497419bcmsh33bfdf2c7ae66afp17d3e2jsnb7047bd63e48",
-            "X-RapidAPI-Host": "meta-llama-fast-api.p.rapidapi.com"
-        }
